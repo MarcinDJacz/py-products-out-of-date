7dac65da9ce277e29926328d02ca1e4b42487a6c
Marcin Djaczuk <marcindjaczuk@gmail.com>
2025-03-16 13:01:42 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,31 +1,72 @@
 from app.main import outdated_products
 import datetime
 from unittest.mock import patch, Mock
+import pytest
 
 
-@patch("app.main.datetime.date")
-def test_outdated_products(mocked_date: Mock) -> None:
-    mocked_date.today.return_value = datetime.datetime(2000, 1, 1)
-    all_products = [
-        {
-            "name": "salmon",
-            "expiration_date": datetime.date(2022, 2, 10),
-            "price": 600
-        },
-        {
-            "name": "chicken",
-            "expiration_date": datetime.date(2022, 2, 5),
-            "price": 120
-        },
-        {
-            "name": "duck",
-            "expiration_date": datetime.date(2022, 2, 1),
-            "price": 160
-        }
+@pytest.mark.parametrize(
+    "product_list, today_date, expected",
+    [
+        (
+            [
+                {
+                    "name": "salmon",
+                    "expiration_date": datetime.date(2022, 2, 10),
+                    "price": 600},
+                {
+                    "name": "chicken",
+                    "expiration_date": datetime.date(2022, 2, 5),
+                    "price": 120},
+                {
+                    "name": "duck",
+                    "expiration_date": datetime.date(2022, 2, 1),
+                    "price": 160
+                },
+            ],
+            datetime.date(2022, 2, 2),
+            ["duck"],
+
+        ),
     ]
-    result = outdated_products(all_products)
-    assert result == [x["name"] for x in all_products]
-    mocked_date.today.return_value = datetime.datetime(2025, 1, 1)
+)
+@patch("app.main.datetime.date")
+def test_outdated_products(mocked_date: Mock,
+                           product_list: dict,
+                           today_date: datetime,
+                           expected: list) -> None:
+    mocked_date.today.return_value = today_date
+    assert outdated_products(product_list) == expected
 
-    result = outdated_products(all_products)
-    assert result == []
+
+@pytest.mark.parametrize(
+    "product_list, today_date, expected",
+    [
+        (
+            [
+                {
+                    "name": "salmon",
+                    "expiration_date": datetime.date(2022, 2, 10),
+                    "price": 600},
+                {
+                    "name": "chicken",
+                    "expiration_date": datetime.date(2022, 2, 5),
+                    "price": 120},
+                {
+                    "name": "duck",
+                    "expiration_date": datetime.date(2022, 2, 1),
+                    "price": 160
+                },
+            ],
+            datetime.date(2022, 2, 10),
+            ["chicken", "duck"],
+
+        ),
+    ]
+)
+@patch("app.main.datetime.date")
+def test_outdated_products_equal_date(mocked_date: Mock,
+                                      product_list: dict,
+                                      today_date: datetime,
+                                      expected: list) -> None:
+    mocked_date.today.return_value = today_date
+    assert outdated_products(product_list) == expected
